2c228b716d1bce2d7f3cd12ccc7a515fc689bb7b
Sanh Vu <sanhvu020@gmail.com>
2022-10-27 16:32:03 -0400

    commit tree 52107087ef4c0dd2786f0bb0f67d44a4ac5fd9c0

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+let messages = [];
+function writeCards(name, event) {
+  for (let count = 0; count < name.length; count++) {
+    messages.push(`Thank you, ${name[count]}, for the wonderful ${event} gift!`)
+
+}
+return messages;
+}
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+
+}
